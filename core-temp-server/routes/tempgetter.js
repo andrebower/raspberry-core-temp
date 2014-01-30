817bb4070f0366219eb22d417395e3ba5127e5ec
ht <dfg@we.de>
2014-01-30 18:39:51 +0100

    initial commit

---
+++ core-temp-server/routes/tempgetter.js
@@ -0,0 +1,31 @@
+function getJsonFromUrl(url, res) {
+    var http = require('http');
+    http.get(url,function (result) {
+        var body = '';
+
+        result.on('data', function (chunk) {
+            body += chunk;
+        });
+
+        result.on('end', function () {
+            res.send(JSON.stringify(JSON.parse(body)));
+
+        });
+    }).on('error', function (e) {
+        console.log("Got error: ", e);
+    })
+
+}
+/**
+ * Created by andrebauer on 30.01.14.
+ */
+
+exports.listXBMC = function(req, res){
+    var url = 'http://192.168.1.102:4000/temps';
+    getJsonFromUrl(url, res);
+}
+exports.listPI = function(req, res){
+    var url = 'http://localhost:4000/temps';
+    getJsonFromUrl(url, res);
+}
+
---
+++ core-temp-service/routes/tempService.js
@@ -0,0 +1,70 @@
+/**
+ * Created by andrebauer on 30.01.14.
+ */
+
+exports.saveCoreTemp = function (){
+
+    var exec=require('child_process').exec;
+    exec('cat /sys/class/thermal/thermal_zone0/temp',function(err,stdout){
+        if(err){
+
+            var temp=(Math.random()*100).toFixed(2);
+            writeToDB(temp);
+
+        }else{
+            var temp=(parseInt(stdout)/1000).toFixed(2);
+            writeToDB(temp);
+        }
+    });
+}
+
+exports.coreTemps = function(req, res){
+
+    var fs = require("fs");
+    var file = "test.db";
+    var exists = fs.existsSync(file);
+
+    if(!exists) {
+        console.log("No DatabaseFile");
+        res.send("");
+        return;
+    }
+
+    var sqlite3 = require("sqlite3").verbose();
+    var db = new sqlite3.Database(file);
+
+    var jsonTemps = {values:[]};
+    var time = new Date().getTime() - 1000*60*60*12;
+    db.all("SELECT timestamp ,temp  FROM temps WHERE timestamp > "+ time +" ORDER BY timestamp", function(err, rows) {
+        rows.forEach(function (row) {
+            jsonTemps.values.push([row.timestamp,row.temp]);
+        });
+        res.send(JSON.stringify(jsonTemps));
+    });
+    db.close();
+
+};
+
+
+function writeToDB(temp){
+    var fs = require("fs");
+    var file = "test.db";
+    var exists = fs.existsSync(file);
+
+    if(!exists) {
+        console.log("Creating DB file.");
+        fs.openSync(file, "w");
+    }
+
+    var sqlite3 = require("sqlite3").verbose();
+    var db = new sqlite3.Database(file);
+
+    db.serialize(function() {
+        if(!exists) {
+            db.run("CREATE TABLE temps (timestamp DATETIME, temp NUMERIC)");
+        }
+        var stmt = db.prepare("INSERT INTO temps VALUES (?,?)");
+        stmt.run(new Date().getTime(),temp);
+        stmt.finalize();
+    });
+}
